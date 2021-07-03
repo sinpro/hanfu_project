662487bb9ff724d3d74df699319bd22dffa3ca59
sinpro <1040680627@qq.com>
2021-07-03 17:36:03 +0800

    fix:修改editor富文本框

---
+++ src/components/editor/index copy 3.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div id="editorMenu" class="editorMenu"></div>
+        <div id="editor" class="editor"></div>
+    </div>
+</template>
+<script>
+import E from 'wangeditor'
+export default {
+  name: 'editor',
+  props: {
+    value: {
+      default: ""
+    }
+  },
+  mounted () {
+    var editor = new E('#editorMenu', '#editor');
+    editor.create();
+    this.editor.customConfig.onchange = (html) => { // 获取内容
+      this.$emit("input", html);
+    };
+  }
+}
+</script>
+<style lang="scss" scoped>
+.editorMenu {
+  border: 1px solid #ccc;
+}
+.editor { 
+  margin-top: -1px; /*将多出来的一像素边框隐藏 */
+  border: 1px solid #ccc;
+  min-height: 400px; /*//编辑框最小高度*/
+  height:auto; /*编辑框高度超过最小高度会根据内容高度自适应*/
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="index">
-    2321332
+    <div class="editor">
+      <editor v-model="detail" :isClear="isClear" @change="change"></editor>
+    </div>
+    
   </div>
 </template>
 <script>
+import editor from "src/components/editor";
 export default {
   name:"Index",
   data(){
-      return {
-
-      }
+    return {
+      isClear: false,
+      detail:"213123312"
+    }
   },
   components:{
+    editor
   },
   mounted(){
     
   },
   methods:{
-    
+    change(val) {
+      console.log(val)
+      
+    },
   }
 }
 </script>
